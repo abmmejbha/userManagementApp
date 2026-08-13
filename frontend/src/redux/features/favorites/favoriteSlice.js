0f63c52eab8b0cd50ce50fec5298811aa07d8686
A.B.M. Mejbha Saimum Chapol <146228288+abmmejbha@users.noreply.github.com>
2026-08-13 22:26:43 +0600

    Implement favorites slice with add/remove reducers

--- frontend/src/redux/features/favorites/favoriteSlice.js
+++ frontend/src/redux/features/favorites/favoriteSlice.js
@@ -1,1 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
 
+const favoriteSlice = createSlice({
+  name: "favorites",
+  initialState: [],
+  reducers: {
+    addToFavorites: (state, action) => {
+      if (!state.some((p) => p._id === action.payload._id)) {
+        state.push(action.payload);
+      }
+    },
+    removeFromFavorites: (state, action) => {
+      return state.filter((p) => p._id !== action.payload._id);
+    },
+  },
+});
+
+export const { addToFavorites, removeFromFavorites } = favoriteSlice.actions;
+export default favoriteSlice.reducer;
